4bc60f0c43be51dda33d996914945b1d1be8ef9e
Rohan Paul <rohanpaul2@gmail.com>
2018-07-28 23:59:27 +0530

    ES 6 Prototype function definition

---
+++ Topic-Wise-Notes/Javascript/Prototypal-Inheritence/ES6-Function-definition-With-Class.js
@@ -0,0 +1,34 @@
+/*
+ * Quick example on how to use the new class feature of ES6
+ * instead of the prototype-based object-oriented programming
+ */
+
+ class Person {
+     constructor(name) {
+         this.name = name;
+         console.log("Creating a new Person")
+     }
+
+     hello() {
+         console.log(`Hello ${this.name} you are a person`);
+     }
+ }
+
+ class Worker extends Person {
+     constructor(name, job) {
+         super(name);
+         this.job = job;
+         console.log("Creating a new Worker")
+     }
+
+     hello() {
+         console.log(`Hello ${this.name} you are a worker`)
+     }
+ }
+// class definition ends here
+
+var p1 = new Person("Rohan");
+var w1 = new Worker("Paul");
+
+p1.hello()  // => Hello Rohan you are a person
+w1.hello()  // => Hello Paul you are a worker
--- Topic-Wise-Notes/Javascript/Prototypal-Inheritence/Prototype-Example-1.js
+++ Topic-Wise-Notes/Javascript/Prototypal-Inheritence/Prototype-Example-1.js
@@ -1,9 +1,36 @@
-// Extending Native Constructors.  This is often considered a bad practice because if you create a custom String method and browsers in the future implement that method slightly differently, code may not work as you’d expect. Despite that, it is still a good exercise and worthwhile to learn for understanding prototypes.
-
-// Write a function to replace a single space with a "-"
-
-String.prototype.dasherize = function() {
-    return this.replace(/\s/g, '-')
+var PersonProto = function (name) {
+    this.name = name;
+    console.log("Creating a new Person the prototype way")
 }
 
-console.log('Hello world'.dasherize());
+PersonProto.prototype.hello = function () {
+    console.log(`Hello ${this.name} You are part of this PersonProto`)
+}
+
+
+// To define a derived class first , A) Create a new class and call the constructor of the base class explicitly with the `Class.call`. At least one parameter must be passed to that: the current object wih the `this` keyword
+
+var WorkerProto = function (name, job) {
+    PersonProto.call(this, name);
+
+    this.job = job;
+    console.log("Creating a new workder")
+}
+// B) Create the prototype of the derived class, based on the base class. To do that, *hard* copy the prototype of the base class. Don't use neither the simple assignment operator nor `new BaseClass()` !
+
+WorkerProto.prototype = Object.create(PersonProto.prototype);
+
+// C)(Re)set the `constructor` property to refer to the new class instead of the (copied) base class.
+
+WorkerProto.prototype.constructor = WorkerProto;
+
+// Overwrite inherited functions or create new ones if you want.
+WorkerProto.prototype.hello = function () {
+    console.log(`Hello ${this.name} You are part of this WorkerProto`)
+}
+
+var p1Proto = new PersonProto("Jack");
+var w1Proto = new WorkerProto("Bill");
+
+p1Proto.hello()
+w1Proto.hello()
