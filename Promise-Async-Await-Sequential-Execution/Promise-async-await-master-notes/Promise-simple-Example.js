936f3dcad8aba65786da0e30ab7a4fbbec1a8634
Rohan Paul <rohanpaul2@gmail.com>
2019-05-12 20:47:11 +0530

    Promise example

---
+++ Promise-Async-Await-Sequential-Execution/Promise-async-await-master-notes/Promise-simple-Example.js
@@ -0,0 +1,89 @@
+const isMomHappy = true;
+
+// Define a function to return a Promise
+const willIGetNewPhone = new Promise((resolve, reject) => {
+	if (isMomHappy) {
+		const phone = {
+			brand: "Samsung",
+			color: "black"
+		};
+		resolve(phone);
+	} else {
+		const reason = new Error("Mon is not happy");
+		reject(reason);
+	}
+});
+
+const showOff = phone => {
+	const message =
+		"Hey friend, I have a new " +
+		phone.color +
+		" " +
+		phone.brand +
+		" phone";
+	return Promise.resolve(message);
+};
+
+// call out promise
+const askMom = () => {
+	willIGetNewPhone
+		.then(showOff)
+		.then(fullfilled => console.log(fullfilled))
+		.then(error => console.log(error.message));
+};
+
+askMom();
+
+// Same above example with ES-8 async-await
+//
+// const isMomHappy = true;
+
+// Promise
+const willIGetNewPhone = new Promise((resolve, reject) => {
+	if (isMomHappy) {
+		const phone = {
+			brand: "Samsung",
+			color: "black"
+		};
+		resolve(phone);
+	} else {
+		const reason = new Error("mom is not happy");
+		reject(reason);
+	}
+});
+
+// 2nd promise
+async function showOff(phone) {
+	return new Promise((resolve, reject) => {
+		var message =
+			"Hey friend, I have a new " +
+			phone.color +
+			" " +
+			phone.brand +
+			" phone";
+
+		resolve(message);
+	});
+}
+
+// call our promise
+async function askMom() {
+	try {
+		console.log("before asking Mom");
+
+		let phone = await willIGetNewPhone;
+		let message = await showOff(phone);
+
+		console.log(message);
+		console.log("after asking mom");
+	} catch (error) {
+		console.log(error.message);
+	}
+}
+
+(async () => {
+	await askMom();
+})();
+
+/*Further Reading - 
+[https://scotch.io/tutorials/javascript-promises-for-dummies](https://scotch.io/tutorials/javascript-promises-for-dummies)*/
