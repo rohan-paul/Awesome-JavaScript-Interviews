352d208ea99f74a1c8f563134bcd081607409c6a
Rohan Paul <rohanpaul2@gmail.com>
2020-02-26 02:11:21 +0530

    more super useful daily techniques on rest and spread

--- Javascript/rest-spread-destructuring/rest-spread.js
+++ Javascript/rest-spread-destructuring/rest-spread.js
@@ -1,8 +1,8 @@
-/* KEY POINT - The spread operator allows us to spread the value of an array (or any iterable) across zero or more arguments in a function or elements in an array (or any iterable). 
+/* KEY POINT - The spread operator allows us to spread the value of an array (or any iterable) across zero or more arguments in a function or elements in an array (or any iterable).
 
-SO WITH SPREAD THING 'UNPACKING ELEMENTS' - Allowes an iterable (array, string, object) to be expanded where more arguments / elements are expected. 
+SO WITH SPREAD THING 'UNPACKING ELEMENTS' - Allowes an iterable (array, string, object) to be expanded where more arguments / elements are expected.
 
-The rest parameter allows us to pass an indefinite number of parameters to a function and access them in an array. So rest parameter is ONLY about when I am implementing argument passing mechanism in more compact way - Otherwise both take the same triple-dot (...) syntax 
+The rest parameter allows us to pass an indefinite number of parameters to a function and access them in an array. So rest parameter is ONLY about when I am implementing argument passing mechanism in more compact way - Otherwise both take the same triple-dot (...) syntax
 Rest parameters must be at the end or it does not work
 
 SO WITH REST THING 'PACKING ELEMENTS' - Collects multiple elements and condenses them into a single eleme.
@@ -34,7 +34,42 @@ let newObj = {
 
 console.log(newObj);    // => { key1: 'value1', key2: 'value2', key3: 'value3' }
 
-// Example - 3  - Spreading Array
+// Example - 3 - MERGE OBJECTS - Spread properties also provide a new way to merge two or more objects, which can be used as an alternative to the Object.assign() method: - This is VERY IMPORTANT
+
+const obj1 = {a: 10};
+const obj2 = {b: 20};
+const obj3 = {c: 30};
+
+// ES2018
+console.log({...obj1, ...obj2, ...obj3});    // → {a: 10, b: 20, c: 30}
+
+// ES2015
+console.log(Object.assign({}, obj1, obj2, obj3));    // → {a: 10, b: 20, c: 30}
+
+// Example - 4 - It's important to remember that spread properties only copy enumerable properties. In the following example, the type property won’t show up in the copied object because its enumerable attribute is set to false:
+
+const car = {
+  color: 'blue'
+};
+
+Object.defineProperty(car, 'type', {
+  value: 'coupe',
+  enumerable: false
+});
+
+console.log({...car});    // → {color: "blue"}
+
+// Example - 5  - Keep in mind that spread properties can only make a shallow copy of an object. If a property holds an object, only the reference to the object will be copied:
+
+const obj = {x: {y: 10}};
+const copy1 = {...obj};
+const copy2 = {...obj};
+
+console.log(copy1.x === copy2.x);    // → true
+
+/* The x property in copy1 refers to the same object in memory that x in copy2 refers to, so the strict equality operator returns true. */
+
+// Example - 6  - Spreading Array
 
 //Combine two arrays.
 
@@ -52,5 +87,56 @@ const newArr = [...animals.slice(0, 3), ...animals.slice(4)];
 
 console.log(newArr); // [ 'squirrel', 'bear', 'deer', 'rat' ]
 
+// Example - 7 - Very useful feature added to ES2015 was rest parameters, which enabled JavaScript programmers to use ... to represent values as an array. For example:
 
+const arr = [10, 20, 30];
+const [x, ...rest] = arr;
+
+console.log(x);       // → 10
+console.log(rest);    // → [20, 30]
+
+/* Here, the first item in arr is assigned to x, and remaining elements are assigned to the rest variable. This pattern, called array destructuring, became so popular that the Ecma Technical Committee decided to bring similar functionality to objects: */
+
+// Example - 8  - Rest parameter to objects - VERY IMPORTANT
+
+const obj = {
+  a: 10,
+  b: 20,
+  c: 30
+};
+
+const {a, ...rest} = obj;
+
+console.log(a);       // → 10
+console.log(rest);    // → {b: 20, c: 30}
+
+/* The above code uses the rest properties in a destructuring assignment to copy the remaining own enumerable properties into a new object. Note that rest properties must always appear at the end of the object, otherwise an error is thrown: */
+
+const obj = {
+  a: 10,
+  b: 20,
+  c: 30
+};
+
+const {...rest, a} = obj;    // → SyntaxError: Rest element must be last element
+
+/* Example - 9 Also keep in mind that using multiple rest syntaxes in an object causes an error unless they are nested: */
+
+
+const obj = {
+  a: 10,
+  b: {
+    x: 20,
+    y: 30,
+    z: 40
+  }
+};
+
+const {b: {x, ...rest1}, ...rest2} = obj;    // no error
+
+const {...rest, ...rest2} = obj;    // → SyntaxError: Rest element must be last element
+
+/* Further Reading
+[css-tricks.com/new-es2018-features-every-javascript-developer-should-know/](https://css-tricks.com/new-es2018-features-every-javascript-developer-should-know/)
+ */
 
